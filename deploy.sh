87fca752a77d6e98a56727bd866b562ec295fb78
Alexander Wittig <alexander@wittig.name>
2018-07-17 17:03:25 +0100

    test

--- deploy.sh
+++ deploy.sh
@@ -1,9 +1,11 @@
 #!/bin/sh
 cd ${TRAVIS_BUILD_DIR}/build
-git clone -b pkg-latest https://abgandar:${GITHUB_TOKEN}@github.com/abgandar/dace.git pkg-latest
+#git clone -b pkg-latest https://abgandar:${GITHUB_TOKEN}@github.com/abgandar/dace.git pkg-latest
+git clone --branch pkg-latest https://github.com/abgandar/dace.git pkg-latest
 cd pkg-latest
 cp ${TRAVIS_BUILD_DIR}/build/packages/* .
-git commit -a -m 'Update build artifacts'
-git push
+git -q add *
+git -q commit -a -m 'Update build artifacts for commit ${TRAVIS_COMMIT}'
+git -q push
 cd ..
 rm -rf pkg-latest
