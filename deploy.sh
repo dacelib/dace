bda617e78051500d8bf5aa0ed5638fb5589fee3b
Alexander Wittig <alexander@wittig.name>
2018-07-17 17:08:56 +0100

    fix

--- deploy.sh
+++ deploy.sh
@@ -1,7 +1,6 @@
 #!/bin/sh
 cd ${TRAVIS_BUILD_DIR}/build
-#git clone -b pkg-latest https://abgandar:${GITHUB_TOKEN}@github.com/abgandar/dace.git pkg-latest
-git clone --branch pkg-latest https://github.com/abgandar/dace.git pkg-latest
+  git clone -b pkg-latest https://abgandar:${GITHUB_TOKEN}@github.com/abgandar/dace.git pkg-latest
 cd pkg-latest
 cp ${TRAVIS_BUILD_DIR}/build/packages/* .
 git add *
