a20d1a3dd29c069589dcf670c2426502a7514cf8
Alexander Wittig <abgandar@gmail.com>
2018-08-15 12:37:47 +0100

    Travis CI support (#9)
    Add Travis CI support

---
+++ deploy.sh
@@ -0,0 +1,10 @@
+#!/bin/sh
+cd ${TRAVIS_BUILD_DIR}/build
+  git clone -b pkg-latest https://abgandar:${GITHUB_TOKEN}@github.com/abgandar/dace.git pkg-latest >/dev/null 2>&1
+cd pkg-latest
+cp ${TRAVIS_BUILD_DIR}/build/packages/* .
+git add *
+git commit -a -m "Automatic update of build artifacts for commit ${TRAVIS_COMMIT}"
+git push
+cd ..
+rm -rf pkg-latest
